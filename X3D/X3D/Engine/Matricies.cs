0cceaf55484e7037e84e215648eda926414b857f
Gerallt Franke <Gerallt Franke>
2016-07-05 23:41:10 +1000

    Implementation of transform

---
+++ X3D/X3D/Engine/Matricies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace X3D
+{
+    public class Matricies
+    {
+        public Matrix4 modelview;
+        public Matrix4 projection;
+
+        public Vector3 Scale = Vector3.One; // accumulated scale
+        //private Quaternion _orientation; // TODO: use Quaternions to model rotation
+    }
+}
