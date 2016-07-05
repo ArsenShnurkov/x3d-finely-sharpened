9b4371bb56fcbe62a4f5ba8b0c499928c9ac34f1
Gerallt Franke <Gerallt Franke>
2016-07-05 23:40:19 +1000

    Implementation of transform

---
+++ X3D/X3D/Core/Transform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace X3D
+{
+    public partial class Transform
+    {
+        public override void Render(RenderingContext rc)
+        {
+            base.Render(rc);
+
+            rc.Translate(this.Translation);
+            rc.Scale(this.Scale);
+            rc.Rotate(this.Rotation);
+
+            rc.PushMatricies();
+        }
+
+        public override void PostRender(RenderingContext rc)
+        {
+            base.PostRender(rc);
+
+            rc.PopMatricies();
+        }
+    }
+}
