7acd2847e8f71708627a75fab58c5321e2159ad9
Gerallt Franke <geralltf@gmail.com>
2016-07-20 01:25:15 +1000

    fix to stop the way OpenFileDialog handles downloads and caches of URLs

--- x3druntime.ui.opentk/App/App.cs
+++ x3druntime.ui.opentk/App/App.cs
@@ -27,7 +27,8 @@ namespace x3druntime.ui.opentk
             using (var p = Process.GetCurrentProcess())
                 GetWindowThreadProcessId(p.MainWindowHandle, out pid);
 
-            var hHook = SetWinEventHook(EVENT_OBJECT_VALUECHANGE, EVENT_OBJECT_VALUECHANGE, IntPtr.Zero, CallWinEventProc, pid, tid, WINEVENT_OUTOFCONTEXT);
+            var hHook = SetWinEventHook(EVENT_OBJECT_VALUECHANGE, EVENT_OBJECT_INVOKED, 
+                                        IntPtr.Zero, CallWinEventProc, pid, tid, WINEVENT_OUTOFCONTEXT);
 
 
             #endregion
@@ -53,7 +54,9 @@ namespace x3druntime.ui.opentk
             fileui.RestoreDirectory = true;
             fileui.Multiselect = false;
 
-            if (fileui.ShowDialog(X3DProgram.CurrentProgram) == DialogResult.OK)
+            DialogResult result = fileui.ShowDialog(X3DProgram.CurrentProgram);
+
+            if (result == DialogResult.OK)
             {
 
                 string temporaryInternetFilesDir = Environment.GetFolderPath(System.Environment.SpecialFolder.InternetCache);
@@ -69,9 +72,18 @@ namespace x3druntime.ui.opentk
                     url = fileui.FileName;
                 }
             }
+            else if (result == DialogResult.Cancel && OpenFileCancelURLDownload)
+            {
+                url = OpenFileDialogURL;
+            }
 
             UnhookWinEvent(hHook);
             fileui.Dispose();
+
+            if (string.IsNullOrEmpty(url.Trim()))
+            {
+                url = null;
+            }
 
             return url;
         }
@@ -88,23 +100,90 @@ namespace x3druntime.ui.opentk
         private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
         [DllImport("user32.dll", SetLastError = true)]
         private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Auto)]
+        static extern int SendMessage3(IntPtr hwndControl, uint Msg,int wParam, StringBuilder strBuffer); // get text
+        [DllImport("user32.dll", EntryPoint = "SendMessage",CharSet = CharSet.Auto)]
+        static extern int SendMessage4(IntPtr hwndControl, uint Msg,int wParam, int lParam);  // text length
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetParent(IntPtr hWnd);
 
+        public static void CloseWindow(IntPtr hwnd)
+        {
+            SendMessage4(hwnd, WM_CLOSE, 0, 0);
+        }
+
+        private const UInt32 WM_CLOSE = 0x0010;
 
         private const int WINEVENT_OUTOFCONTEXT = 0;
+
+        /// <summary>
+        /// If a textbox or scrollbar has changed
+        /// </summary>
         private const uint EVENT_OBJECT_VALUECHANGE = 0x800E;
+        
+        /// <summary>
+        /// If a button has been clicked
+        /// </summary>
+        private const uint EVENT_OBJECT_INVOKED = 0x8013;
 
         private static String OpenFileDialogURL = "";
+        private static bool OpenFileCancelURLDownload = false;
         private static WinEventProc CallWinEventProc = new WinEventProc(EventCallback);
         private delegate void WinEventProc(IntPtr hWinEventHook, int iEvent, IntPtr hWnd, int idObject, int idChild, int dwEventThread, int dwmsEventTime);
-        private static void EventCallback(IntPtr hWinEventHook, int iEvent, IntPtr hWnd, int idObject, int idChild, int dwEventThread, int dwmsEventTime)
+
+        private static int GetTextBoxTextLength(IntPtr hTextBox)
+        {
+            // helper for GetTextBoxText
+            uint WM_GETTEXTLENGTH = 0x000E;
+            int result = SendMessage4(hTextBox, WM_GETTEXTLENGTH,
+              0, 0);
+            return result;
+        }
+
+        private static string GetTextBoxText(IntPtr hTextBox)
+        {
+            uint WM_GETTEXT = 0x000D;
+            int len = GetTextBoxTextLength(hTextBox);
+            if (len <= 0) return null;  // no text
+            StringBuilder sb = new StringBuilder(len + 1);
+            SendMessage3(hTextBox, WM_GETTEXT, len + 1, sb);
+            return sb.ToString();
+        }
+
+        private static void EventCallback(IntPtr hWinEventHook, int iEvent, IntPtr hWnd, int idObject, int idChild, 
+                                            int dwEventThread, int dwmsEventTime)
         {
             StringBuilder sb1 = new StringBuilder(256);
             GetClassName(hWnd, sb1, sb1.Capacity);
+            string eventClass = sb1.ToString();
+
+            switch (eventClass)
+            {
+                case "Button":
+
+                    string label = GetTextBoxText(hWnd).ToLower();
+
+                    if(label.Contains("open") || label.Contains("ok") && OpenFileCancelURLDownload)
+                    {
+                        IntPtr winHwnd = GetParent(hWnd);
+
+                        // Cancel window quickly to prevent download and cache of URL that OpenFileDialog does
+                        CloseWindow(winHwnd);
+                    }
+
+                    break;
+                case "Edit":
+
+                    StringBuilder sb = new StringBuilder(512);
+                    GetWindowText(hWnd, sb, sb.Capacity);
+                    OpenFileDialogURL = sb.ToString();
+                    OpenFileCancelURLDownload = true;
+
+                    break;
+            }
             if (sb1.ToString() == "Edit")
             {
-                StringBuilder sb = new StringBuilder(512);
-                GetWindowText(hWnd, sb, sb.Capacity);
-                OpenFileDialogURL = sb.ToString();
+
             }
         }
 
