f516048954f0eab1f050f8ebd9f0680f322a39da
Gerallt Franke <geralltf@gmail.com>
2016-07-20 22:02:46 +1000

    NavigationInfo type attribute implementation

---
+++ X3D/X3D/Core/NavigationInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X3D.Engine;
+
+namespace X3D
+{
+    public partial class NavigationInfo
+    {
+        public static NavigationType NavigationType = NavigationType.Examine;
+
+        public override void Load()
+        {
+            base.Load();
+
+            string navType = this.type.ToUpper();
+             
+            if (navType.Contains("ANY"))
+            {
+                NavigationType = NavigationType.Examine;
+            }
+            else if (navType.Contains("EXAMINE"))
+            {
+                NavigationType = NavigationType.Examine;
+            }
+            else if (navType.Contains("WALK"))
+            {
+                NavigationType = NavigationType.Walk;
+            }
+            else if (navType.Contains("FLY"))
+            {
+                NavigationType = NavigationType.Fly;
+            }
+        }
+    }
+}
