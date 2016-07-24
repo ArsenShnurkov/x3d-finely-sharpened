b140c16c1245e454ebee637df5cf71ee7c58b1c5
Gerallt Franke <geralltf@gmail.com>
2016-07-24 11:15:54 +1000

    added initial switch node

---
+++ X3D/X3D/Core/Switch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X3D
+{
+    public partial class Switch
+    {
+
+        #region Rendering Methods
+
+        public override void Load()
+        {
+            base.Load();
+
+
+        }
+
+        public override void Render(RenderingContext rc)
+        {
+            base.Render(rc);
+
+
+        }
+
+        #endregion
+    }
+}
