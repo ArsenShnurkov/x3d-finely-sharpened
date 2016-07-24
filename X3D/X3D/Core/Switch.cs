fc1fbd6ef76228bcde608feb4798ea9a2e1372c2
Gerallt Franke <geralltf@gmail.com>
2016-07-24 16:29:33 +1000

    implemented switch node

--- X3D/X3D/Core/Switch.cs
+++ X3D/X3D/Core/Switch.cs
@@ -5,23 +5,75 @@ using System.Text;
 
 namespace X3D
 {
+    /// <summary>
+    /// http://www.web3d.org/files/specifications/19775-1/V3.3/Part01/components/group.html#Switch
+    /// </summary>
     public partial class Switch
     {
 
+        public Switch()
+        {
+            // The switch will take care of its own rendering
+            //this.PassthroughAllowed = false;
+            this.debug = true;
+        }
+
         #region Rendering Methods
 
-        public override void Load()
+        public override void SwitchNode(int choice)
         {
-            base.Load();
+            if (debug && Shadow != null && Shadow.Count > 0)
+            {
+                if (choice >= 0 && choice < Shadow.Count)
+                {
+                    Console.WriteLine("switching to child {0} {1}", Shadow[choice], Shadow[choice]._id);
+                }
+                else
+                {
+                    Console.WriteLine("switching to child {0} {1}", Shadow[choice], Shadow[choice]._id);
+                }
+            }
 
+            if (Shadow != null && Shadow.Count > 0)
+            {
+                this.Children.Clear();
 
+                this.Children.Add(this.Shadow[choice]);
+            }
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            // Use the Children property conceptually as a visibility list 
+            // where only the current visible item is used rendered at a time.
+
+            CopyToShadowDom();
+
+            if(Shadow != null && Shadow.Count > 0)
+                this.Children.Add(this.Shadow[this.WhichChoice]);
         }
 
         public override void Render(RenderingContext rc)
         {
             base.Render(rc);
 
+            /* Because passthrough is not allowed, this shape must make sure one of its children are rendered */
+            //if (this.Shadow != null && Shadow.Count > 0)
+            //{
 
+            //    if (_whichChoice >= 0 && _whichChoice < this.Shadow.Count)
+            //    {
+            //        SceneGraphNode n; // the node that is now visible to the engine
+
+            //        n = this.Shadow[WhichChoice];
+
+
+            //        if(PassthroughAllowed == false)
+            //            n.Render(rc);
+            //    }
+            //}
         }
 
         #endregion
