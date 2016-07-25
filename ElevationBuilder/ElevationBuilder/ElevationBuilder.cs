a7d0f729c3a98493a7bec5752e7eaa06ed349aeb
Gerallt Franke <geralltf@gmail.com>
2016-07-26 02:09:10 +1000

    added ElevationBuilder project

---
+++ ElevationBuilder/ElevationBuilder/ElevationBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace X3D
+{
+    public delegate Vector3 ElevationColorSequencer(int face, int vertex);
+
+    public class ElevationBuilder
+    {
+
+
+        public ElevationGrid BuildFlatPlane(int xDimension, int zDimension, 
+                                            float xSpacing, float zSpacing, bool colorPerVertex,
+                                            ElevationColorSequencer colorSequencer)
+        {
+            ElevationGrid g = new ElevationGrid();
+            Color colorNode = new Color();
+            int numHeights = xDimension * zDimension;
+            int[] heights = new int[numHeights];
+            int faceIndex = 0;
+            int coordIndex = 0;
+            Vector3 color;
+
+            g.colorPerVertex = colorPerVertex;
+            g.xDimension = xDimension.ToString();
+            g.zDimension = zDimension.ToString();
+            g.xSpacing = xSpacing;
+            g.zSpacing = zSpacing;
+            g.height = string.Empty;
+            colorNode.color = string.Empty;
+
+            for (coordIndex = 0; coordIndex < numHeights; coordIndex++)
+            {
+                if(coordIndex % 4 == 0 && coordIndex > 0)
+                {
+                    faceIndex++;
+                }
+
+                g.height += " 0";
+
+                color = colorSequencer(faceIndex, coordIndex);
+
+                colorNode.color += string.Format(" {0} {1} {2}, ", color.X, color.Y, color.Z);
+            }
+
+            colorNode.color = colorNode.color.TrimStart();
+            colorNode.color = colorNode.color.Substring(0, colorNode.color.Length - 2);
+
+            g.height = g.height.TrimStart();
+
+            
+            g.Children.Add(colorNode);
+
+            return g;
+        }
+    }
+}
---
+++ ElevationBuilder/ElevationBuilder/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using OpenTK;
+using X3D.Engine;
+using System.Reflection;
+using X3D.Parser;
+
+namespace X3D
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ElevationBuilder by Gerallt Franke 2013 - 2016");
+            Console.WriteLine("Builds an ElevationGrid given parameters outputting in X3D XML encoding");
+
+            X3DSceneGraphXMLSerializer serializer;
+            ElevationBuilder builder = new ElevationBuilder();
+            ElevationGrid elevation;
+            X3D root = new X3D();
+            string xml;
+
+            // Build a big chessboard as a test, then save test to x3d file
+
+            elevation = builder.BuildFlatPlane(100, 100, 1f, 1f, false, colorSequencer);
+
+            root.Children.Add(elevation);
+
+            // Build XML from all the SceneGraphNodes
+            serializer = new X3DSceneGraphXMLSerializer(root);
+            xml = serializer.Serialize();
+
+
+            Console.WriteLine("\n~~~ X3D Gemerated Below ~~~\n");
+            Console.WriteLine(xml);
+
+            File.WriteAllText("D:\\out.x3d", xml);
+
+            Console.ReadLine();
+        }
+
+        public static Vector3 colorSequencer(int face, int vertex)
+        {
+            Vector3 color = new Vector3();
+
+            if(vertex % 2 == 0)
+            {
+                color = Vector3.Zero; // black
+                color = new Vector3(OpenTK.Graphics.Color4.DarkGreen.R,
+                                    OpenTK.Graphics.Color4.DarkGreen.G,
+                                    OpenTK.Graphics.Color4.DarkGreen.B);
+            }
+            else
+            {
+                color = Vector3.One; // white
+            }
+
+            return color;
+        }
+
+
+    }
+}
