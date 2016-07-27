615f1517858caedb424beba401e97b42cbee8a0a
Gerallt Franke <geralltf@gmail.com>
2016-07-27 18:32:45 +1000

    updated to include height map generation from on the fly perlin noise

--- ElevationBuilder/ElevationBuilder/ElevationBuilder.cs
+++ ElevationBuilder/ElevationBuilder/ElevationBuilder.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenTK;
+﻿using OpenTK;
+using System;
+using System.Drawing;
+using X3D.Engine;
 using X3D.Parser;
-using System.Reflection;
 
 namespace X3D
 {
@@ -21,14 +17,98 @@ namespace X3D
     /// </summary>
     public delegate float HeightComputationDelegate(int x, int z);
 
-    public interface IElevationBuilder
+    public class ElevationBuilder
     {
 
+        public ElevationGrid BuildHeightmapFromPerlin(RenderingContext rc, PerlinNoise perlin, out Bitmap largePerlinImage)
+        {
+            ElevationGrid g;
+            Bitmap noiseBitmap;
+            Image newImage;
 
-    }
+            Console.WriteLine("Rendering perlin noise");
+            noiseBitmap = largePerlinImage = perlin.GetPerlinNoise(rc);
 
-    public class ElevationBuilder : IElevationBuilder
-    {
+            newImage = noiseBitmap.GetThumbnailImage(500, 500, null, IntPtr.Zero); // scale image to make processing easier
+            //noiseBitmap.Dispose();
+            noiseBitmap = (Bitmap)newImage;
+
+            g = BuildHeightmapFromTexture(1, 1, noiseBitmap, 250.0f); // build a rather large height map
+
+            return g;
+        }
+
+        public ElevationGrid BuildHeightmapFromTexture(
+                                            float xSpacing, float zSpacing, 
+                                            Bitmap texture, 
+                                            float maxHeight = 1.0f)
+        {
+            ElevationGrid g = new ElevationGrid();
+            Color colorNode = new Color();
+            int xDimension, zDimension;
+            int numHeights;
+            int faceIndex = 0;
+            int coordIndex = 0;
+            float height;
+            System.Drawing.Color color;
+            Vector3 vColor;
+            Vector3 vNTSCConversionWeight;
+            float grayscale;
+
+            g.colorPerVertex = true;
+            g.xSpacing = xSpacing;
+            g.zSpacing = zSpacing;
+            g.height = string.Empty;
+            colorNode.color = string.Empty;
+
+            xDimension = texture.Width;
+            zDimension = texture.Height;
+
+            numHeights = xDimension * zDimension;
+
+            g.xDimension = xDimension.ToString();
+            g.zDimension = zDimension.ToString();
+
+            Console.WriteLine("Building height map from texture (this could take a while)");
+
+            for (int x = 0; x < xDimension; x++)
+            {
+                for (int z = 0; z < zDimension; z++)
+                {
+                    if (coordIndex % 4 == 0 && coordIndex > 0)
+                    {
+                        faceIndex++;
+                    }
+
+                    color = texture.GetPixel(x, z);
+
+
+                    // Convert to grayscale using NTSC conversion weights
+                    vColor = new Vector3(color.R / 255f, color.G / 255f, color.B / 255f);
+                    vNTSCConversionWeight = new Vector3(0.299f, 0.587f, 0.114f);
+                    grayscale = Vector3.Dot(vColor, vNTSCConversionWeight);
+
+
+                    // Convert color to height value
+                    height = (float)(grayscale) * maxHeight;
+
+                    g.height += string.Format(" {0}", height);
+                    colorNode.color += X3DTypeConverters.ToString(vColor) + ", ";
+
+                    updateProgress(coordIndex, numHeights);
+
+                    coordIndex++;
+                }
+            }
+
+            colorNode.color = colorNode.color.Trim();
+            colorNode.color = colorNode.color.Substring(0, colorNode.color.Length - 2);
+
+            g.height = g.height.TrimStart();
+            g.Children.Add(colorNode);
+
+            return g;
+        }
 
 
         public ElevationGrid BuildCheckerboard(int xDimension, int zDimension,
@@ -63,7 +143,6 @@ namespace X3D
             ElevationGrid g = new ElevationGrid();
             Color colorNode = new Color();
             int numHeights = xDimension * zDimension;
-            int[] heights = new int[numHeights];
             int faceIndex = 0;
             int coordIndex = 0;
             Vector3 color;
@@ -75,6 +154,8 @@ namespace X3D
             g.zSpacing = zSpacing;
             g.height = string.Empty;
             colorNode.color = string.Empty;
+
+            Console.WriteLine("Building height map flat plane (this could take a while)");
 
             for (int x = 0; x < xDimension; x++)
             {
@@ -89,6 +170,8 @@ namespace X3D
                     g.height += " 0";
                     color = colorSequencer(faceIndex, coordIndex, x, z);
                     colorNode.color += X3DTypeConverters.ToString(color) + ", ";
+
+                    updateProgress(coordIndex, numHeights);
 
                     coordIndex++;
                 }
@@ -124,6 +207,8 @@ namespace X3D
             g.height = string.Empty;
             colorNode.color = string.Empty;
 
+            Console.WriteLine("Building varying height map (this could take a while)");
+
             for (int x = 0; x < xDimension; x++)
             {
                 for (int z = 0; z < zDimension; z++)
@@ -141,6 +226,8 @@ namespace X3D
                         colorNode.color += X3DTypeConverters.ToString(color) + ", ";
                     }
 
+                    updateProgress(coordIndex, numHeights);
+
                     coordIndex++;
                 }
             }
@@ -154,6 +241,18 @@ namespace X3D
             return g;
         }
 
+        private void updateProgress(int coordIndex, int numHeights)
+        {
+
+            float percent = ((float)(coordIndex + 1) / (float)numHeights) * 100;
+
+            if (percent > 0 && percent % 1.0f == 0)
+            {
+                Console.SetCursorPosition(0, 15);
+                Console.Write(" {0}%", percent);
+            }
+        }
+
         public void BuildShapeDom(out X3D root, out Shape shape)
         {
             head head = new head();
@@ -164,32 +263,13 @@ namespace X3D
             shape = new Shape();
 
             meta.name = "generator";
-            meta.content = AppInfo;
+            meta.content = BuilderApplication.AppInfo;
 
             scene.Children.Add(shape);
             head.Children.Add(meta);
             
             root.Children.Add(scene);
             root.Children.Add(head);
-        }
-
-        private static string AppInfo
-        {
-            get
-            {
-                Assembly asm;
-                AssemblyProductAttribute productName;
-                AssemblyVersionAttribute ver;
-                AssemblyDescriptionAttribute desc;
-
-                asm = Assembly.GetAssembly(typeof(Parser.XMLParser));
-                productName = (AssemblyProductAttribute)Attribute.GetCustomAttribute(asm, typeof(AssemblyProductAttribute));
-                //ver=(AssemblyVersionAttribute)Attribute.GetCustomAttribute(asm,typeof(AssemblyVersionAttribute));
-                desc = (AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(asm, typeof(AssemblyDescriptionAttribute));
-
-                string version = asm.GetName().Version.ToString();
-                return productName.Product + "_" + version + "_graph_builder";
-            }
         }
     }
 }
