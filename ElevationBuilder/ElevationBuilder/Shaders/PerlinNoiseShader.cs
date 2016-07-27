bcde4f253d614026201f2eedf81f810a57403af5
Gerallt Franke <geralltf@gmail.com>
2016-07-27 18:23:17 +1000

    added perlin noise shaders

---
+++ ElevationBuilder/ElevationBuilder/Shaders/PerlinNoiseShader.cs
@@ -0,0 +1,8 @@
+﻿namespace X3D.Core.Shading
+{
+    public class PerlinNoiseShader
+    {
+        public static string vertexShaderSource = ShaderHelpers.getShaderSource("PerlinNoiseVertexShader.shader");
+        public static string fragmentShaderSource = ShaderHelpers.getShaderSource("PerlinNoiseFragmentShader.shader");
+    }
+}
