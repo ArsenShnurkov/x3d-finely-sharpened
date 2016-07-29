9db44c268a5b6ad5edb05f44878b35dc93b2fa1c
Gerallt Franke <geralltf@gmail.com>
2016-07-29 14:40:36 +1000

    Integrated ConstructionSet with runtime

--- ConstructionSet/Program.cs
+++ ConstructionSet/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace X3D
+﻿using System;
+
+namespace X3D.ConstructionSet
 {
     public class Program
     {
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("The Finely Sharpened Construction Set by Gerallt Franke 2013 - 2016");
+
             BuilderApplication app = BuilderApplication.CreateBuilderApplication();
 
             //OpenTK.Graphics.GraphicsContext context = OpenTK.Graphics.GraphicsContext.CreateDummyContext();
