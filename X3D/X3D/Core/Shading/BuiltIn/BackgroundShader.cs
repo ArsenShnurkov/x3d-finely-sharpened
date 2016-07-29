38d43740781997809f663913b182bec251272b50
Gerallt Franke <geralltf@gmail.com>
2016-07-29 14:50:27 +1000

    Implementation of X3DBackgroundNode shader

---
+++ X3D/X3D/Core/Shading/BuiltIn/BackgroundShader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X3D.Core.Shading
+{
+    public class BackgroundShader
+    {
+
+        private static string @base = AppDomain.CurrentDomain.BaseDirectory + "..\\..\\Shaders\\";
+
+        public static string vertexShaderSource = ShaderCompiler.GetShaderSource("BackgroundVertexShader.shader", @base);
+        public static string fragmentShaderSource = ShaderCompiler.GetShaderSource("BackgroundFragmentShader.shader", @base);
+
+    }
+}
