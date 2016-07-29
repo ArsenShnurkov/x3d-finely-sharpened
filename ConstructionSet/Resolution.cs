1fcdcbec4ce11ee7ec5ac814888cf363111d2d2a
Gerallt Franke <geralltf@gmail.com>
2016-07-29 14:36:15 +1000

    Integrated ConstructionSet with runtime

---
+++ ConstructionSet/Resolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X3D.ConstructionSet
+{
+    public sealed class Resolution
+    {
+        public static Resolution Size800x600 { get { return new Resolution(800, 600); } }
+
+        public static Resolution Size1024x768 { get { return new Resolution(1024, 768); } }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Resolution(int w, int h)
+        {
+            this.Width = w;
+            this.Height = h;
+        }
+    }
+}
