bd0ec5f1e253dcb8b9da3b67ba3920b0bc2e6c52
Gerallt Franke <geralltf@gmail.com>
2016-08-01 05:34:30 +1000

    Core prototyping nodes

---
+++ X3D/X3D/Core/Prototyping/ProtoInstance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using X3D.Parser;
+
+namespace X3D
+{
+    public partial class ProtoInstance
+    {
+        public override void Load()
+        {
+            base.Load();
+
+
+        }
+
+        public override void Render(RenderingContext rc)
+        {
+            base.Render(rc);
+
+
+        }
+    }
+}
