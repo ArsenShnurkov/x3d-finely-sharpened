6e7d2c80db250ba696401f94e3a1dfc405923f21
Gerallt Franke <geralltf@gmail.com>
2016-08-01 05:48:03 +1000

    refactor for proto

---
+++ X3D/X3D/Core/Prototyping/field.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X3D
+{
+    /// <summary>
+    /// Used by Script, ComposedShader and ProtoInterface 
+    /// to provide an API for the event graph and for scripting.
+    /// </summary>
+    public partial class field
+    {
+        private ComposedShader parentShader;
+        private ProtoInterface parentProtoInterface;
+
+        public field()
+        {
+            this.accessType = "inputOutput";
+        }
+
+        public override void Load()
+        {
+            base.Load();
+
+            parentShader = GetParent<ComposedShader>();
+            parentProtoInterface = GetParent<ProtoInterface>();
+
+            if (parentShader != null)
+            {
+                parentShader.Fields.Add(this);
+            }
+        }
+    }
+}
