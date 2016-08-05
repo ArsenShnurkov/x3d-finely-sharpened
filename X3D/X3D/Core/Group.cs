60f31332a30f97b2f41dea51b22a1a7ad4e29d2f
Gerallt Franke <geralltf@gmail.com>
2016-08-05 23:45:17 +1000

    added group node

---
+++ X3D/X3D/Core/Group.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using System.Xml.Serialization;
+
+namespace X3D
+{
+    public partial class Group
+    {
+        private bool _isHidden;
+
+        /// <summary>
+        /// If true, disabled the node from any Render() calls. KeepAlive() is then called instead.
+        /// </summary>
+        [XmlIgnore]
+        public new bool Hidden
+        {
+            get
+            {
+                return _isHidden;
+            }
+            set
+            {
+                _isHidden = value;
+
+                this.Decendants().ForEach(n => n.Hidden = value);
+            }
+        }
+    }
+}
