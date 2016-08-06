e2c7f4dfc49328a1362305a737557696c0048ae9
Gerallt Franke <geralltf@gmail.com>
2016-08-07 00:07:46 +1000

    Added engine enums

---
+++ X3D/X3D/Engine/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X3D.Engine
+{
+    public enum GraphQueryTraversalType
+    {
+        DepthFirst,
+        BredthFirst
+    }
+}
