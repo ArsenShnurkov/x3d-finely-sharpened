57a7323b90df3253e131431fc34c61ea87f0f07b
Gerallt Franke <geralltf@gmail.com>
2016-09-07 01:18:37 +1000

    refactored Sphere into IsoSphere

---
+++ X3D/X3D/Core/IsoSphere.cs
@@ -0,0 +1,108 @@
+﻿// TODO: calculate texcoords using spherical equation in shader
+
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics.OpenGL4;
+using X3D.Core;
+using OpenTK.Input;
+using X3D.Core.Shading.DefaultUniforms;
+using X3D.Core.Shading;
+using X3D.Parser;
+
+namespace X3D
+{
+    /// <summary>
+    /// A Sphere with underlying geometry implemented as an Iso-Sphere.
+    /// No inbuilt tessellator.
+    /// </summary>
+    public partial class IsoSphere : X3DGeometryNode
+    {
+        internal PackedGeometry _pack;
+
+        private Shape parentShape;
+
+        public override void CollectGeometry(
+                            RenderingContext rc,
+                            out GeometryHandle handle,
+                            out BoundingBox bbox,
+                            out bool coloring,
+                            out bool texturing)
+        {
+            handle = GeometryHandle.Zero;
+            bbox = BoundingBox.Zero;
+            coloring = false;
+            texturing = false;
+
+            parentShape = GetParent<Shape>();
+
+            _pack = new PackedGeometry();
+            _pack.restartIndex = -1;
+            _pack._indices = Faces;
+            _pack._coords = Verts;
+            _pack.bbox = BoundingBox.CalculateBoundingBox(Verts);
+            
+            _pack.Interleave();
+
+            // BUFFER GEOMETRY
+            handle = Buffering.BufferShaderGeometry(_pack);
+        }
+
+        #region Rendering Methods
+
+        public override void Render(RenderingContext rc)
+        {
+            //var scale = new Vector3(0.04f, 0.04f, 0.04f);
+
+            //rc.cam.Scale = scale;
+        }
+        
+        #endregion
+
+        #region Icosahedron Geometry
+
+        int[] Faces = new int[] 
+        {
+            2, 1, 0, -1, 
+            3, 2, 0, -1,
+            4, 3, 0, -1,
+            5, 4, 0, -1,
+            1, 5, 0, -1,
+            11, 6,  7, -1,
+            11, 7,  8, -1,
+            11, 8,  9, -1,
+            11, 9,  10, -1,
+            11, 10, 6, -1,
+            1, 2, 6, -1,
+            2, 3, 7, -1,
+            3, 4, 8, -1,
+            4, 5, 9, -1,
+            5, 1, 10, -1,
+            2,  7, 6, -1,
+            3,  8, 7, -1,
+            4,  9, 8, -1,
+            5, 10, 9, -1,
+            1, 6, 10, -1,
+        };
+ 
+        //TODO: scale values correctly
+        Vector3[] Verts = new Vector3[] 
+        {
+             new Vector3(0.000f,  0.000f,  1.000f),
+             new Vector3(0.894f,  0.000f,  0.447f),
+             new Vector3(0.276f,  0.851f,  0.447f),
+            new Vector3(-0.724f,  0.526f,  0.447f),
+            new Vector3(-0.724f, -0.526f,  0.447f),
+             new Vector3(0.276f, -0.851f,  0.447f),
+             new Vector3(0.724f,  0.526f, -0.447f),
+            new Vector3(-0.276f,  0.851f, -0.447f),
+            new Vector3(-0.894f,  0.000f, -0.447f),
+            new Vector3(-0.276f, -0.851f, -0.447f),
+             new Vector3(0.724f, -0.526f, -0.447f),
+             new Vector3(0.000f,  0.000f, -1.000f)
+        };
+        #endregion
+    }
+}
