f908fd08173421f8d06b51c3aea9d6484a1a4a99
Gerallt Franke <geralltf@gmail.com>
2016-10-02 13:01:08 +1100

    Rigid body physics architecture

--- X3D/X3D/Core/RigidBodyPhysics/Physics.cs
+++ X3D/X3D/Core/RigidBodyPhysics/Physics.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,44 @@ using System.Threading.Tasks;
 
 namespace X3D.Core.RigidBodyPhysics
 {
+    public enum ForceType
+    {
+        Force,
+        Impulse,
+        Velocity,
+        Acceleration
+    }
+
     /// <summary>
     /// http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/rigid_physics.html#ConceptsOverview
     /// </summary>
     public class Physics
     {
+        public static Vector3 Gravity; 
 
+        public static void ApplyForce(Vector3 translation, Vector3 force)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ApplyForceImpulse(Vector3 translation, Vector3 force)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ApplyVelocity(Vector3 translation, Vector3 velocity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ApplyAcceleration(Vector3 translation, Vector3 acceleration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ApplyGravityAcceler(Vector3 translation, Vector3 gravity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ X3D/X3D/Core/RigidBodyPhysics/RigidBody.cs
@@ -0,0 +1,97 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X3D.Core.RigidBodyPhysics;
+
+namespace X3D
+{
+    public partial class RigidBody
+    {
+        public Vector3 GetTranslation()
+        {
+            List<Transform> transformHierarchy;
+
+            transformHierarchy = this.AscendantByType<Transform>();
+
+
+
+            throw new NotImplementedException();
+        }
+
+        public void AddForce(Vector3 force, ForceType type)
+        {
+            switch (type)
+            {
+                case ForceType.Force:
+                    ApplyForce(force);
+                    break;
+                case ForceType.Acceleration:
+                    ApplyAcceleration(force);
+                    break;
+                case ForceType.Impulse:
+                    ApplyForceImpulse(force);
+                    break;
+                case ForceType.Velocity:
+                    ApplyVelocity(force);
+                    break;
+            }
+        }
+
+        public void ApplyForce(Vector3 force)
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyForce(translation, force);
+        }
+
+        public void ApplyForceImpulse(Vector3 force)
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyForceImpulse(translation, force);
+        }
+
+        public void ApplyVelocity(Vector3 velocity)
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyVelocity(translation, velocity);
+        }
+
+        public void ApplyAcceleration(Vector3 acceleration)
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyAcceleration(translation, acceleration);
+        }
+
+        public void ApplyGravity()
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyGravityAcceler(translation, Physics.Gravity);
+        }
+
+        public void ApplyGravity(Vector3 gravity)
+        {
+            Vector3 translation;
+
+            translation = GetTranslation();
+
+            Physics.ApplyGravityAcceler(translation, gravity);
+        }
+    }
+}
