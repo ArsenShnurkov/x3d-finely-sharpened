4e0d9fdc312edddc310a44dda032dcf6df39bc61
Gerallt Franke <geralltf@gmail.com>
2016-10-02 14:20:46 +1100

    update rigid body physics model

--- X3D/X3D/Core/RigidBodyPhysics/RigidBody.cs
+++ X3D/X3D/Core/RigidBodyPhysics/RigidBody.cs
@@ -9,11 +9,18 @@ using X3D.Core.RigidBodyPhysics;
 namespace X3D
 {
     /// <summary>
+    /// == Rigid Body Physics == for solid non-deformable objects in the Scene Graph. 
     /// Precondition: Evaluate the Physics Model after any keybindings, and scripts have executed, 
     ///               right before the Scene Graph is rendered. 
     /// </summary>
     public partial class RigidBody
     {
+        /// <summary>
+        /// Gets the translation component of the current non-deformable solid object from the current Transform Hierarchy context.
+        /// </summary>
+        /// <returns>
+        /// A Vector3 composing the translation component.
+        /// </returns>
         public Vector3 GetTranslation()
         {
             List<Transform> transformHierarchy;
