beac22e953658468fb9122dd6f7151ea23f1e806
Gerallt Franke <geralltf@gmail.com>
2016-10-02 12:15:32 +1100

    added physics experimentation component

---
+++ X3D/X3D/Core/RigidBodyPhysics/Physics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X3D.Core.RigidBodyPhysics
+{
+    /// <summary>
+    /// http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/rigid_physics.html#ConceptsOverview
+    /// </summary>
+    public class Physics
+    {
+
+    }
+}
